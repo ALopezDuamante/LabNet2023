aebcc294933842eb9ce97151063aba0fe606b7aa
Lopez Duamante Agustin <55216625+ALopezDuamante@users.noreply.github.com>
2023-09-03 20:03:08 -0300

    Cpdigo para punto 2 de la Practica 4
    Se sube codigo para resolucion de la query del punto 2

--- Practica4/Practica4/Program.cs
+++ Practica4/Practica4/Program.cs
@@ -16,12 +16,19 @@ namespace Practica4
             {
                 Console.Clear();
                 Console.WriteLine("Menu Principal\n\nSeleccione una opción:\n");
-                Console.WriteLine("1 - Query 1\n2 - Query 2\n3 - Query 3\n4 - Query 4\n5 - Query 5\n6 - Query 6\n7 - Query 7\n0 - Salir del programa");
+                Console.WriteLine("1 - Devolver Customer" +
+                    "\n2 - Devolver todos los Productos sin stock" +
+                    "\n3 - Devolver todos los Productos sin stock que cuesten más de 3 por unidad" +
+                    "\n4 - Devolver todos los Customers de la Región WA" +
+                    "\n5 - Devolver el primer elemento o nulo de una lista de Productos donde el ID del producto sea igual a 789" +
+                    "\n6 - Devolver nombre de los Customers en Mayuscula y en Minuscula" +
+                    "\n7 - Devolver Customers y Orders donde los customers sean de la Región WA y la fecha de orden sea mayor a 1/1/1997" +
+                    "\n0 - Salir del programa");
                 opcion = Console.ReadLine();
 
                 switch (opcion)
                 {
-                    case "1"://Query 1
+                    case "1"://Punto 1
                         Console.Clear();
                         Console.WriteLine("Query 1\nIngrese ID del objeto Customer que desee mostrar:");
                         string traerCustomerId = Console.ReadLine();
@@ -38,9 +45,25 @@ namespace Practica4
                         }
                        
                         break;
-                    case "2"://Query 2
+                    case "2"://Punto 2
                         Console.Clear();
-                        Console.WriteLine("Query 2");
+                        Console.WriteLine("Query 2\nProductos sin Stock:");
+                        var query2 = db.Products.Where(p => p.UnitsInStock == 0);
+                        if (query2 != null)
+                        {
+                            foreach (var product in query2)
+                            {
+                                Console.WriteLine($"ID: {product.ProductID} | " +
+                                    $"Nombre del Producto: {product.ProductName} | " +
+                                    $"Nombre del Proveedor: {product.Suppliers.CompanyName}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se encuentran Productos sin Stock.");
+                        }
+                        Console.WriteLine("\nPresione Enter para volver al menú principal...");
+                        Console.ReadLine();
                         break;
                     case "3":
                         Console.WriteLine("Query 3");
