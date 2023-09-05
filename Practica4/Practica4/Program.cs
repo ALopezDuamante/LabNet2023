57b05411b95c7757ae3d102b2b080978a33b981c
Lopez Duamante Agustin <55216625+ALopezDuamante@users.noreply.github.com>
2023-09-04 21:16:35 -0300

    Codigo con resolucion de las consignas opcionales
    Se sube codigo con la resolucion de los puntos opcionales

--- Practica4/Practica4/Program.cs
+++ Practica4/Practica4/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Practica4.Data;
+using Practica4.Entities;
 using System;
+using System.ComponentModel;
 using System.Linq;
 
 
@@ -23,14 +25,20 @@ namespace Practica4
                     "\n5 - Devolver el primer elemento o nulo de una lista de Productos donde el ID del producto sea igual a 789" +
                     "\n6 - Devolver nombre de los Customers en Mayuscula y en Minuscula" +
                     "\n7 - Devolver Customers y Orders donde los customers sean de la Región WA y la fecha de orden sea mayor a 1/1/1997" +
-                    "\n0 - Salir del programa");
+                    "\n8 - Devolver los primeros 3 Customers de la Region WA" +
+                    "\n9 - Devolver lista de Productos ordenados por Nombre | " +
+                    "\n10 - Devolver lista de Prodcutos ordenados por UnitsInStock de mayor a menor" +
+                    "\n11 - Devolver las Categorías asociadas a los Productos" +
+                    "\n12 - Devolver el primer Producto de la lista | " +
+                    "\n13 - Devolver los Customers con la cantidad de Ordenes asociadas" +
+                    "\n0 - Salir del Programa");
                 opcion = Console.ReadLine();
 
                 switch (opcion)
                 {
                     case "1"://Punto 1
                         Console.Clear();
-                        Console.WriteLine("Query 1\nIngrese ID del objeto Customer que desee mostrar:");
+                        Console.WriteLine("Query 1\nIngrese ID del objeto Customer que desee mostrar (Formato 'AAAAA'):\n");
                         string traerCustomer = Console.ReadLine();
 
                         var query1 = db.Customers.FirstOrDefault(c => c.CustomerID == traerCustomer);
@@ -42,9 +50,9 @@ namespace Practica4
                         else
                         {
                             Console.WriteLine("Customer no encontrado. Tal vez ingresó un ID incorrecto.\nPresione Enter para volver al menú principal...");
-                            Console.ReadLine();
                         }
-                       
+
+                        Console.ReadLine();
                         break;
 
                     case "2"://Punto 2
@@ -55,7 +63,7 @@ namespace Practica4
 
                         if (query2 != null)
                         {
-                            Console.WriteLine("\nProductos sin Stock:");
+                            Console.WriteLine("\nProductos sin Stock:\n");
                             foreach (var product in query2)
                             {
                                 Console.WriteLine($"ID: {product.ProductID} | " +
@@ -81,7 +89,7 @@ namespace Practica4
 
                         if (query3 != null)
                         {
-                            Console.WriteLine("\nProductos con Stock con valor de más de 3 por unidad:");
+                            Console.WriteLine("\nProductos con Stock con valor de más de 3 por unidad:\n");
                             foreach (var product in query3)
                             {
                                 Console.WriteLine($"ID: {product.ProductID} | " +
@@ -106,7 +114,7 @@ namespace Practica4
 
                         if (query4 != null)
                         {
-                            Console.WriteLine("\nCustomers de la Region WA:");
+                            Console.WriteLine("\nCustomers de la Region WA:\n");
                             foreach (var customer in query4)
                             {
                                 Console.WriteLine($"Id: {customer.CustomerID} | " +
@@ -131,7 +139,7 @@ namespace Practica4
 
                         if (query5 != null)
                         {
-                            Console.WriteLine("\nProducto con ID = 789:");
+                            Console.WriteLine("\nProducto con ID = 789:\n");
                             Console.WriteLine($"Nombre del Producto: {query5.ProductName} | " +
                                 $"Stock: {query5.UnitsInStock} | " +
                                 $"Precio por Unidad: {query5.UnitPrice}");
@@ -154,7 +162,7 @@ namespace Practica4
 
                         if (query6 != null)
                         {
-                            Console.WriteLine("Nombre de los Customer en mayúcula y minúscula");
+                            Console.WriteLine("\nNombre de los Customer en mayúcula y minúscula\n");
                             foreach (var customer in query6)
                             {
                                 Console.WriteLine($"Nombre en Mayúscula: {customer.CompanyName.ToUpper()} | " +
@@ -170,7 +178,7 @@ namespace Practica4
                         Console.ReadLine();
                         break;
 
-                    case "7"://Punto
+                    case "7"://Punto 7
                         Console.Clear();
                         Console.WriteLine("Query 7");
 
@@ -182,12 +190,12 @@ namespace Practica4
 
                         if (query7 != null)
                         {
-                            Console.WriteLine("\nCustomers cuya Region es WA y la Fecha de Orden es mayor a 1/1/1997:");
-                            foreach (var item in query7)
+                            Console.WriteLine("\nCustomers cuya Region es WA y la Fecha de Orden es mayor a 1/1/1997:\n");
+                            foreach (var customer in query7)
                             {
-                                Console.WriteLine($"Nombre de la Compañía: {item.customerName} | " +
-                                    $"Telefono: {item.customerPhone} | "+
-                                    $"Fecha de Orden: {item.orderDate}");
+                                Console.WriteLine($"Nombre de la Compañía: {customer.customerName} | " +
+                                    $"Telefono: {customer.customerPhone} | "+
+                                    $"Fecha de Orden: {customer.orderDate}");
                             }
                         }
                         else
@@ -197,6 +205,152 @@ namespace Practica4
 
                         Console.WriteLine("\nPresione Enter para volver al menú principal...");
                         Console.ReadLine ();
+                        break;
+
+                    case "8"://Punto 8
+                        Console.Clear();
+                        Console.WriteLine("Query 10");
+
+                        var query8 = db.Customers.Where(c => c.Region == "WA").Take(3);
+
+                        if (query8 != null)
+                        {
+                            foreach (var customer in query8)
+                            {
+                                Console.WriteLine($"ID: {customer.CustomerID} | " +
+                                    $"Nombre de la Compañïa: {customer.CompanyName} | " +
+                                    $"Region: {customer.Region}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se encontraron Customers cuya Region sea WA");
+                        }
+
+                        Console.WriteLine("\nPresione Enter para volver al menú principal...");
+                        Console.ReadLine();
+                        break;
+
+                    case "9"://Punto 9
+                        Console.Clear();
+                        Console.WriteLine("Query 9");
+
+                        var query9 = db.Products.OrderBy(p => p.ProductName).ToList();
+
+                        if (query9 != null)
+                        {
+                            Console.WriteLine("\nLista de Productos ordenados por Nombre:\n");
+                            foreach (var product in query9)
+                            {
+                                Console.WriteLine($"ID: {product.ProductID} | " +
+                                    $"Nombre del Producto: {product.ProductName} | " +
+                                    $"Unidades en Stock: {product.UnitsInStock}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se enconrtaron Productos en la Base de Datos");
+                        }
+
+                        Console.WriteLine("\nPresione Enter para volver al menú principal...");
+                        Console.ReadLine();
+                        break;
+
+                    case "10"://Punto 10
+                        Console.Clear();
+                        Console.WriteLine("Query 10");
+
+                        var query10 = (from product in db.Products
+                                      orderby product.UnitsInStock descending
+                                      select product).ToList();
+
+                        if (query10 != null)
+                        {
+                            foreach (var product in query10)
+                            {
+                                Console.WriteLine($"ID: {product.ProductID} | " +
+                                        $"Nombre del Producto: {product.ProductName} | " +
+                                        $"Unidades en Stock: {product.UnitsInStock}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se enconrtaron Productos en la Base de Datos");
+                        }
+
+                        Console.WriteLine("\nPresione Enter para volver al menú principal...");
+                        Console.ReadLine();
+                        break;
+
+                    case "11"://Punto 11
+                        Console.Clear();
+                        Console.WriteLine("Query 11");
+
+                        var query11 = db.Products.Select(p => p.Categories).GroupBy(p => p.CategoryID).ToList();
+
+                        if (query11!= null)
+                        {
+                            Console.WriteLine("\nLista de Categorias asociadas a Productos:\n");
+                            foreach(var productCategory in query11)
+                            {
+                                Console.WriteLine($"ID del Prodcuto {productCategory.First().CategoryID} | " +
+                                    $"ID de Categoria: {productCategory.First().CategoryName}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No hay Categorias asociadas a Productos");
+                        }
+
+                        Console.WriteLine("\nPresione Enter para volver al menú principal...");
+                        Console.ReadLine();
+                        break;
+
+                    case "12"://Punto 12
+                        Console.Clear();
+                        Console.WriteLine("Query 12");
+
+                        var query12 = db.Products.First();
+
+                        if (query12 != null)
+                        {
+                            Console.WriteLine("\nPrimer Producto de la lista:\n");
+                            Console.WriteLine($"ID: {query12.ProductID} | " +
+                                $"Nombre del Producto: {query12.ProductName} | " +
+                                $"Unidades en Stock: {query12.UnitsInStock}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("La lista está vacía");
+                        }
+
+                        Console.WriteLine("\nPresione Enter para volver al menú principal...");
+                        Console.ReadLine();
+                        break;
+
+                    case "13"://Punto 13
+                        Console.Clear();
+                        Console.WriteLine("Query 13");
+
+                        var query13 = db.Customers.Select(c => new { customer = c, orderCount = c.Orders.Count() }).ToList();
+
+                        if (query13 != null)
+                        {
+                            Console.WriteLine("\nLista de Customers con su cantidad de Ordenes:\n");
+                            foreach (var item in query13)
+                            {
+                                Console.WriteLine($"Customer ID: {item.customer.CustomerID} | " +
+                                    $"Nombre de Compañía: {item.customer.CompanyName} | " +
+                                    $"Cantidad de Ordenes: {item.orderCount}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nNo se encontraron datos\n");
+                        }
+
+                        Console.WriteLine("\nPresione Enter para volver al menú principal...");
+                        Console.ReadLine();
                         break;
 
                     case "0":
